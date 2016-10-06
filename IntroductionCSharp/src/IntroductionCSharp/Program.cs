00a9777aa787c10c9a760a339056843cc77b2104
FELIPE FRIZZO <FFRIZZO2@univel.univel.br>
2016-10-06 20:26:20 -0300

    Introduction CSharp

---
+++ IntroductionCSharp/src/IntroductionCSharp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntroductionCSharp
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            double n = 0, res = 0;
+            Console.WriteLine("Digite um numero: \n");
+            n = Double.Parse(Console.ReadLine());
+			if (n > 0) {
+				res = Math.Sqrt(n);
+				Console.WriteLine("A raiz de {0} é: {0:0.00}", n, res);	
+				
+				res = Math.Pow(n, 2.00);
+				Console.WriteLine("O quadrado de {0} é: {0:0.00}", n, res);	
+				
+				res = Math.Pow(n, (1.00 / 3.00));
+				Console.WriteLine("A raiz cúbica de {0} é: {0:0.00}", n, res);	
+			} else {
+				Console.WriteLine("Não foi possivel calcular!\n");
+			}
+            Console.ReadKey(); 
+        }
+    }
+}
---
+++ IntroductionCSharp2/src/IntroductionCSharp2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace IntroductionCSharp2
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            double n1 = 0, n2 = 0, res = 0;
+
+            try
+            {
+                Console.WriteLine("Digite o valor 01:");
+                n1 = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o valor 02:");
+                n2 = Double.Parse(Console.ReadLine());
+
+                if (n2 != 0)
+                {
+                    res = n1 / n2;
+                    MessageBox.Show(
+                        "Resultado = " + String.Format("{0:0.00}", res),
+                        "Resultado",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information
+                    );
+                } else {
+                    MessageBox.Show(
+                        "Não há divisão por zero!",
+                        "Atenção",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
+                    );
+                }
+            } catch {
+                MessageBox.Show(
+                    "Você informou números inválidos!", 
+                    "Atenção", 
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+            }
+        }
+    }
+}
