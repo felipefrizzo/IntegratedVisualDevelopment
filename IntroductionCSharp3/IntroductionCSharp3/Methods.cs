84bbfc05767e45d0b29b2172ee70e4cdc316feef
FELIPE FRIZZO <FFRIZZO2@univel.univel.br>
2016-10-10 21:43:08 -0300

    Introduction CSharp3

---
+++ IntroductionCSharp3/IntroductionCSharp3/Methods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace IntroductionCSharp3
+{
+    public class Methods
+    {
+        public void  fillIn(int[] v, int t)
+        {
+            for (int i = 0; i < t; i++)
+            {
+                Console.WriteLine("Digite o numero {0} de {1}", i+1, t);
+                v[i] = Int32.Parse(Console.ReadLine());
+                Console.Clear();
+            }        
+        }
+        public void print(int[] v, int t)
+        {
+            for (int i = 0; i < t; i++)
+            {
+                if ((i + 1) == t){
+                    Console.WriteLine("{0}. ", v[i]);
+                } else {
+                    Console.WriteLine("{0}, ", v[i]);
+                }
+
+            }
+        }
+    }
+}
---
+++ IntroductionCSharp3/IntroductionCSharp3/Principal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace IntroductionCSharp3
+{
+    class Principal
+    {
+        static void Main(string[] args)
+        {
+            const int t = 7;
+            int[] vet = new int[t];
+
+            Methods methods = new Methods();
+            methods.fillIn(vet, t);
+            methods.print(vet, t);
+            Console.ReadKey();
+        }
+    }
+}
