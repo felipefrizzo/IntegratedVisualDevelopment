29d20d1a768fca5bc0274e24374cdcd46ffb2ed6
Felipe Frizzo <felipefrizzo@gmail.com>
2016-11-17 20:33:09 -0200

    Create insert on Save Button Click

--- ProjectLayers/ProjectLayers/Cadastro.cs
+++ ProjectLayers/ProjectLayers/Cadastro.cs
@@ -7,14 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ProjectLayers.Code.BLL;
+using ProjectLayers.Code.DTO;
 
-namespace ProjectLayers
-{
-    public partial class Clientes : Form
-    {
-        public Clientes()
-        {
+namespace ProjectLayers {
+    public partial class Clientes : Form {
+        ClientBLL bll = new ClientBLL();
+        ClientDTO dto = new ClientDTO();
+
+        public Clientes() {
             InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e) {
+            dto.Name = tbName.Text;
+            dto.Birthday = tbBirthday.Text;
+            dto.Phone = tbPhone.Text;
+
+            bll.insert(dto);        
         }
     }
 }
